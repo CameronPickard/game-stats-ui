939da4dda6051ed45ffe43805f828b85428d0f8c
Cameron Pickard <cpickard828@gmail.com>
2026-01-11 14:02:32 -0500

    CICD test attempt 4

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Blah test change 3. Edit <code>src/App.jsx</code> and save to test HMR
+          Blah test change 4. Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
